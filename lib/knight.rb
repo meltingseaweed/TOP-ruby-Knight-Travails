a2554abedd743282059bbacfdde0479c856dff18
meltingseaweed <conor.seo@gmail.com>
2026-03-28 15:01:16 +0900

    Refactor: Knight class
    Make the code in Knight class simpler and with
    less instance variables. Also remove simple
    methods to reduce lines of code

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,7 +1,5 @@
 require 'pry-byebug'
 
-# module KnightTravails
-
 class Knight
 
   attr_reader :start, :previous
@@ -9,38 +7,27 @@ class Knight
   def initialize(start = [0,0], goal = [7,7], previous = [])
     @start = start
     @goal = goal
-    @new_positions = []
-    @possible_positions = []
     @previous = previous.dup
     @goal_reached = false
   end
 
-  def add_previous(vertex)
-    @previous << vertex
-  end
-
   def travail
-    @new_positions = []
-    @possible_positions = []
+    new_positions, possible_positions = [], []
     movement_array = [[1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,-1], [-2,1], [-1,2]]
     movement_array.each do |x, y|
-      @possible_positions << [@start[0] + x, @start[1] + y]
+      possible_positions << [@start[0] + x, @start[1] + y]
     end
-    @possible_positions.each do |x, y|
+    possible_positions.each do |x, y|
       if x.between?(0,7) && y.between?(0,7)
-        @new_positions << [x, y]
+        new_positions << [x, y]
       end
     end
     @previous.each do |x, y|
-      @new_positions.delete([x, y])
+      new_positions.delete([x, y])
     end
-    add_previous(@start)
-    return true if goal?
-    @new_positions
-  end
-
-  def goal?
-    @new_positions.include?(@goal)
+    @previous << @start
+    return true if new_positions.include?(@goal)
+    new_positions
   end
 
 end
